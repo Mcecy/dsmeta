8b0126f435ea74421c9f9bf9f54eea82c991cc7f
mcecy <ceciliacadengue@gmail.com>
2022-09-10 18:03:48 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from "./assets/components/Header";
 import SalesCard from "./assets/components/SalesCard";
 
@@ -6,6 +8,7 @@ import './index.css';
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
